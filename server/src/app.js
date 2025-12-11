eb209c100bddd80bc9d55ae9d8f0d8a3c557d3e6
- <->
2025-12-11 16:49:33 +0700

    postmanTest

--- server/src/app.js
+++ server/src/app.js
@@ -1,6 +1,13 @@
 
 let express = require('express')
+let bodyParser = require('body-parser') // เพิ่มบรรทัดนี้
+
 const app = express()
+
+
+// เพิ่ม 2 บรรทัดนี้เพื่อใช้งาน body-parser
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended: true}))
 
 app.get('/status', function (req, res) {
     res.send('Hello nodejs server')
@@ -20,7 +27,20 @@ app.get('/user/:userId', function (req, res) {
 app.get('/users', function (req, res) {
     res.send('เรียกข้อมูล ผู้ใช้งานทั้งหมด')
 })
+// create user
+app.post('/user/', function (req, res) {
+    res.send('ทำการสร้างผู้ใช้งาน: ' + JSON.stringify(req.body))
+})
 
+// edit user
+app.put('/user/:userId', function (req, res) {
+    res.send('ทำการแก้ไขผู้ใช้งาน: ' + req.params.userId + ' : ' + JSON.stringify(req.body))
+})
+
+// delete user
+app.delete('/user/:userId', function (req, res) {
+    res.send('ทำการลบผู้ใช้งาน: ' + req.params.userId + ' : ' + JSON.stringify(req.body))
+})
 let port = 8081
 app.listen(port, function () {
     console.log('server running on ' + port)
