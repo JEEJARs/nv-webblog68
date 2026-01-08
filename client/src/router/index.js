277b513dd005727d66a859a6f57fcfb015ba38dc
nv-webblog68 <chalermchaib66@nu.ac.th>
2026-01-08 17:05:05 +0700

    chap7

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -1,22 +1,20 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import UserCreate from '../components/Users/CreateUser.vue'
+import UserEdit from '../components/Users/EditUser.vue'
+import UserShow from '../components/Users/ShowUser.vue'
+import UserIndex from '../components/Users/Index.vue'
 
-// Import Components
-import UserIndex from '@/components/Users/Index.vue'
-import UserCreate from '@/components/Users/CreateUser.vue'
-import UserEdit from '@/components/Users/EditUser.vue'
-import UserShow from '@/components/Users/ShowUser.vue'
-import Login from '@/components/Login.vue'
-
-// Blogs
-import BlogIndex from '@/components/Blogs/Index.vue'
-import BlogCreate from '@/components/Blogs/CreateBlog.vue'
-import BlogEdit from '@/components/Blogs/EditBlog.vue'
-import BlogShow from '@/components/Blogs/ShowBlog.vue'
 
 const router = createRouter({
   history: createWebHistory(),
   routes: [
     {
+      path: '/',
+      name: 'home',
+      component: () => import('../views/HomeView.vue'),
+    },
+ 
+   {
       path: '/users',
       name: 'users',
       component: UserIndex
@@ -27,42 +25,16 @@ const router = createRouter({
       component: UserCreate
     },
     {
-      path: '/user/edit/:userId',
+      path: '/user/edit/:userId', // เพิ่ม :userId เพื่อรับค่า
       name: 'user-edit',
       component: UserEdit
     },
     {
-      path: '/user/:userId',
+      path: '/user/:userId', // เพิ่ม :userId
       name: 'user',
       component: UserShow
-    },
-    {
-      path: '/login',
-      name: 'login',
-      component: Login
-    },
-    // blogs
-    {
-        path: '/blogs',
-        name: 'blogs',
-        component: BlogIndex
-    },
-    {
-        path: '/blog/create',
-        name: 'blogs-create', // แก้ไขชื่อให้สื่อความหมาย
-        component: BlogCreate
-    },
-    {
-        path: '/blog/edit/:blogId',
-        name: 'blog-edit',
-        component: BlogEdit
-    },
-    {
-        path: '/blog/:blogId',
-        name: 'blog',
-        component: BlogShow
     }
-  ],
+  ]
 })
 
 export default router
