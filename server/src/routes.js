4d3394840ea35b6b91fc39a33cba6a0fab339819
nv-webblog68 <chalermchaib66@nu.ac.th>
2026-01-08 14:42:41 +0700

    aa

--- server/src/routes.js
+++ server/src/routes.js
@@ -1,20 +1,40 @@
+const isAuthenController = require('./controllers/isAuthenController')
 const UserController = require('./controllers/UserController')
+const UserAuthenController = require('./controllers/UserAuthenController')
+const BlogController = require('./controllers/BlogController')
+
+const UploadController = require('./controllers/UploadController')
+const fileUploadMiddleware = require('./middleware/fileUpload') // Import Middleware
 
 module.exports = (app) => {
-    /* RESFUL Api for users management */
-    
-    // create user
-    app.post('/user', UserController.create)
+  // เส้นทางเดิมจากบทที่ 4 (ถ้าอยากเก็บไว้)
+  // app.get('/status', (req, res) => res.send('server is running'))
 
-    // edit user, suspend, active
-    app.put('/user/:userId', UserController.put)
+  // เส้นทางใหม่สำหรับ User Management แบบ MVC
+  app.get('/users', isAuthenController, UserController.index)
+  app.post('/user', UserController.create)
+  app.put('/user/:userId', UserController.put)
+  app.delete('/user/:userId', UserController.remove)
+  app.get('/user/:userId', UserController.show)
+  app.post('/login', UserAuthenController.login)
+  app.post('/register', UserAuthenController.register)
+  // blog route
+  // create blog
+  app.post('/blog', BlogController.create)
 
-    // delete user
-    app.delete('/user/:userId', UserController.remove)
+  // edit blog, suspend, active
+  app.put('/blog/:blogId', BlogController.put)
 
-    // get user by id
-    app.get('/user/:userId', UserController.show)
+  // delete blog
+  app.delete('/blog/:blogId', BlogController.remove)
 
-    // get all user
-    app.get('/users', UserController.index)
+  // get blog by id
+  app.get('/blog/:blogId', BlogController.show)
+
+  // get all blog
+  app.get('/blogs', BlogController.index)
+  
+  // Route สำหรับ Upload โดยเฉพาะ
+  // logic: เรียก middleware ก่อน -> ถ้าผ่าน -> เรียก controller
+  app.post('/upload', fileUploadMiddleware, UploadController.upload)
 }
