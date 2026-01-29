fe3b532ae8c0c1c77aacec03fc04bf334044048d
nv-webblog68 <chalermchaib66@nu.ac.th>
2026-01-29 15:09:55 +0700

    ss

--- client/src/components/Users/Index.vue
+++ client/src/components/Users/Index.vue
@@ -1,20 +1,15 @@
 <template>
   <div>
-    <h1>Get All Users</h1>
-    <div v-if="users.length">
-      <div>จำนวนผู้ใช้งาน {{ users.length }}</div>
-      <div v-for="user in users" v-bind:key="user.id">
-            <p>id: {{ user.id }}</p>
-            <p>ชื่อ-นามสกุล: {{ user.name }} - {{ user.lastname }}</p>
-            <p>email: {{ user.email }}</p>
-            <p>password: {{ user.password }}</p>
-            <hr>
-            <p><button v-on:click="navigateTo('/user/'+user.id)">ดูข้อมูลผู้ใช้</button></p>
-        </div>
-
+    <h2>Get all users</h2>
+    <h4>จำนวนผู้ใช้งาน {{users.length}}</h4>
+    <div v-for="user in users" v-bind:key="user.id">
+      <p>id: {{ user.id }}</p>
+      <p>ชื่อ-นามสกุล: {{ user.name }} - {{ user.lastname }}</p>
+      <p>email: {{ user.email }}</p>
+      <p>password: {{ user.password }}</p>
+      <p><button v-on:click="navigateTo('/user/'+user.id)">ดูข้อมูลผู้ใช้</button></p>
+      <hr>
     </div>
-
-    <p><button @click="navigateTo('/user/create')">สร้างผู้ใช้งาน</button></p>
   </div>
 </template>
 
@@ -22,11 +17,19 @@
 import UsersService from '../../services/UsersService'
 
 export default {
-  data() {
-    return {
-      users: []
-    }
-  },
+    data () {
+        return {
+            users: []
+        }
+    },
+    async created () {
+        try {
+            this.users = (await UsersService.index()).data
+            console.log(this.users)
+        } catch (error) {
+            console.log(error)
+        }
+    },
 
 
   // Logic จะเขียนตรงนี้
@@ -36,16 +39,8 @@ export default {
     },
   },
 
-  async created() {
-    try {
-      this.users = (await UsersService.index()).data
-      console.log(this.users)
-    } catch (error) {
-      console.log(error)
-    }
-  }
-}
 
+};
 </script>
 
 <style scoped>
--- client/src/services/UsersService.js
+++ client/src/services/UsersService.js
@@ -17,4 +17,3 @@ export default {
         return Api().delete('user/'+user.id) // แก้ไข user เป็น user.id ตามหลักการส่ง id
     },
 }
-
